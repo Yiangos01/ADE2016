22d03fc21885768891667e48cd253e8fac70f5ab
Yiangos01 <ygeorg01@cs.ucy.ac.cy>
2016-08-22 20:14:42 +0300

    updated mapper and reducer added

--- mapper.py
+++ mapper.py
@@ -1,1 +1,13 @@
-#mapper
+import sys
+
+mape=open("mapperout.txt",'w')
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    # split the line into words
+    words = line.split()
+    # increase counters
+    for word in words:
+        mape.write(word+"\t1\n")
+mape.close()
