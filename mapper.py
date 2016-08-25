42174ed637d9bc4ec5c5ae25592e8522b33244ad
Yiangos01 <ygeorg01@cs.ucy.ac.cy>
2016-08-25 20:05:49 +0300

    datafile

--- mapper.py
+++ mapper.py
@@ -1,6 +1,6 @@
 import sys
 
-mape=open("mapperout.txt",'w')
+#mape=open("mapperout.txt",'w')
 # input comes from STDIN (standard input)
 for line in sys.stdin:
     # remove leading and trailing whitespace
@@ -9,5 +9,4 @@ for line in sys.stdin:
     words = line.split()
     # increase counters
     for word in words:
-        mape.write(word+"\t1\n")
-mape.close()
+        print(word+"\t1")
